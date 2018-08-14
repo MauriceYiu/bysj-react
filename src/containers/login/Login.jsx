8d64f59575fc049eef3d2b9681efe4e3f955feed
yaohao <maurice-yiu@qq.com>
2018-08-14 17:23:07 +0800

    登录页界面完成

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -5,7 +5,19 @@ class Login extends Component {
     render() {
         return (
             <div className="login">
-                login
+                <div className="login-mask"></div>
+                <div className="login-form">
+                    <p>招聘系统</p>
+                    <div className="username">
+                        <input type="text" placeholder="请输入用户名" name="username" id=""/>
+                    </div>
+                    <div className="password">
+                        <input type="text" placeholder="请输入密码" name="password" id=""/>
+                    </div>
+                    <div className="login-btn">
+                        <button>登录</button>
+                    </div>
+                </div>
             </div>
         );
     }
